ad89b64f0da13ac100535b8abdf9ac3936c3cf0f
Leandro Martin Rios <leaxplosion@gmail.com>
2021-05-03 20:05:51 -0300

    Commit Inicial: Modelo de Datos y Seeds, scripts para crear la base de datos.

---
+++ Modelo de Datos/Seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO coloresgatos (nombre) VALUES ("negro");
+INSERT INTO coloresgatos (nombre) VALUES ("blanco");
+INSERT INTO coloresgatos (nombre) VALUES ("gris");
+
+INSERT INTO estadosgato (nombre) VALUES ("en adopcion");
+INSERT INTO estadosgato (nombre) VALUES ("adoptado");
+INSERT INTO estadosgato (nombre) VALUES ("en tramite");
+
+INSERT INTO estadostramiteadopcion (nombre) VALUES ("finalizado");
+INSERT INTO estadostramiteadopcion (nombre) VALUES ("en tramite");
+INSERT INTO estadostramiteadopcion (nombre) VALUES ("rechazado");
+INSERT INTO estadostramiteadopcion (nombre) VALUES ("iniciado");
