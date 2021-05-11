2e695162eac0b1c5956b4cf7709a1e163a77e712
Leael95 <leaxplosion@gmail.com>
2021-05-11 20:04:49 -0300

    ColoresGatos: validaciones generalizadas

--- Code/Backoffice/VacunasGatos/controller.js
+++ Code/Backoffice/VacunasGatos/controller.js
@@ -17,7 +17,10 @@ function mostrarListado() {
 function enviar() {
     let formulario = document.getElementById("formEditarVacunas");
     let validaciones = [
+        iniciarValidacion("nombreVacuna"),
         estaVacio("nombreVacuna"),
+        validarLongitudMaxima("nombreVacuna", 5),
+        iniciarValidacion("descripcionVacuna"),
         estaVacio("descripcionVacuna")];
 
     
@@ -32,15 +35,45 @@ function estaVacio(nombreCampo) {
     let resultado = false;
     let txt = document.getElementById(nombreCampo);
     let mensajeError = document.getElementById("msjError" + nombreCampo);
+    console.log(txt.esvalido, txt.value, nombreCampo, "Vacio");
 
     if(txt.value == ""){
         mensajeError.classList.add("msjErrorVisible");
         mensajeError.classList.remove("msjErrorInvisible");
         resultado = true;
+        txt.esvalido = false;
     } else {
-        mensajeError.classList.add("msjErrorInvisible");
-        mensajeError.classList.remove("msjErrorVisible");
+        if(txt.esvalido == true){
+            mensajeError.classList.add("msjErrorInvisible");
+            mensajeError.classList.remove("msjErrorVisible");
+        }
     }
 
     return resultado;
+}
+
+function validarLongitudMaxima(nombreCampo, longitudMaxima) {
+    let resultado = false;
+    let txt = document.getElementById(nombreCampo);
+    let mensajeError = document.getElementById("msjError" + nombreCampo);
+    console.log(txt.esvalido, txt.value, nombreCampo, "Longitud Maxima");
+
+    if(txt.value.length > longitudMaxima){
+        mensajeError.classList.add("msjErrorVisible");
+        mensajeError.classList.remove("msjErrorInvisible");
+        resultado = true;
+        txt.esvalido = false;
+    } else {
+        if(txt.esvalido == true){
+            mensajeError.classList.add("msjErrorInvisible");
+            mensajeError.classList.remove("msjErrorVisible");
+        }
+    }
+
+    return resultado;
+}
+
+function iniciarValidacion(nombreCampo) {
+    let txt = document.getElementById(nombreCampo);
+    txt.esvalido = true;
 }
